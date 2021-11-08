da927d6888f0a71c56ceffdc464b152022cc5e92
zhaohaiqiang <zhaohaiqiang@youzan.com>
2021-11-08 14:53:45 +0800

    fix

--- scripts/test_json.js
+++ scripts/test_json.js
@@ -4,28 +4,32 @@ This is a k6 test script that imports the xk6-kafka and
 tests Kafka with a 200 JSON messages per iteration.
 
 */
+import {check} from 'k6';
+import {Counter, Rate, Trend} from 'k6/metrics';
+import {close, consume, received,} from 'k6/x/nsq'; // import mqtt plugin
 
-import {consume, reader} from "k6/x/nsq"; // import kafka extension
-
-const lookupaddress = ["10.215.49.141:4161"];
+const lookupAddress = ["10.215.49.141:4161"];
 const nsqTopic = "sinker_test";
 const nsqChannel = "sinker";
 
-const consumer = reader(nsqTopic, nsqChannel);
+const consumer = consume(lookupAddress, nsqTopic, nsqChannel);
+let subscribe_trend = new Trend('subscribe_time', true);
 
-
+let myCounter = new Counter('my_counter');
+let myRate = new Rate("my_rate");
 export default function () {
-    // for (let index = 0; index < 100; index++) {
-    // Read 10 messages only
-    consume(lookupaddress, consumer, 10,3);
-    // check(messages, {
-    //     "10 messages returned": (msgs) => msgs.length == 10,
-    // });
-    // }
+    let startTime = new Date().getTime();
+    let messages = received(lookupAddress, consumer, 10, 3);
+    let paased = check(messages, {
+        "is content correct": msg => msg != null
+    });
+    myCounter.add(1)
+    myRate.add(paased)
+    subscribe_trend.add(new Date().getTime() - startTime);
 }
 
 export function teardown(data) {
-
+    close(consumer);
 }
 
 
